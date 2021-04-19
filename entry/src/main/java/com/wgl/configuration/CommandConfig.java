9758fddc2f2ce24554bba42a9f396aa8dce7ec9d
AlexWeiWGL <1148159686@qq.com>
2021-04-19 19:12:44 +0800

    update Command Section

--- entry/src/main/java/com/wgl/configuration/CommandConfig.java
+++ entry/src/main/java/com/wgl/configuration/CommandConfig.java
@@ -8,29 +8,38 @@ import java.net.URL;
 public class CommandConfig implements Runnable{
     public enum CommandName{Agriculture_Control_light, Agriculture_Control_Motor};
     public enum Command{ON, OFF};
+    public enum CommandType{Light, Motor};
     private String X_Auth_Token;
     private URL url;
     private Command command;
     private CommandName commandName;
     private boolean isSuccess;
+    private CommandType commandType;
 
-    public CommandConfig(URL url, CommandName commandName, Command command, String X_Auth_Token){
+    public CommandConfig(URL url, CommandName commandName, CommandType commandType, String X_Auth_Token){
         this.url = url;
         this.commandName = commandName;
-        this.command = command;
         this.X_Auth_Token = X_Auth_Token;
+        this.commandType = commandType;
+        isSuccess = false;
     }
 
     public void postCommand() throws IOException {
         HttpURLConnection connection = (HttpURLConnection) this.url.openConnection();
         connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
         connection.setRequestProperty("X-Auth-Token", X_Auth_Token);
         connection.setRequestProperty("Connection", "keep-alive");
+        connection.setRequestProperty("Content-Type", "text/plain");
         String requestBody =
                 "{" +
                 "\"service_id\":" + "\"Agriculture\"," +
-                "\"command_name\":" + "\"" + commandName.toString() + "\"" +
-                        "\"paras\":{ \"value\":\"" + command.toString() + "\"}}";
+                "\"command_name\":" + "\"" + commandName.toString() + "\"," +
+                        "\"paras\":{\""+commandType.toString()+"\":\"" + command.toString() + "\"}}";
+
+        connection.setRequestProperty("Content-Length", requestBody.getBytes().length+"");
+
         OutputStream os = connection.getOutputStream();
         os.write(requestBody.getBytes());
         os.flush();
@@ -39,6 +48,7 @@ public class CommandConfig implements Runnable{
         connection.connect();
 
         int resultCode = connection.getResponseCode();
+        System.out.println(resultCode);
         if(resultCode == HttpURLConnection.HTTP_OK){
             isSuccess = true;
         }else{
@@ -57,5 +67,9 @@ public class CommandConfig implements Runnable{
 
     public synchronized boolean checkSuccess(){
         return isSuccess;
+    }
+
+    public synchronized void setCommand(Command command){
+        this.command = command;
     }
 }
--- entry/src/main/java/com/wgl/wearabledesign/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/wgl/wearabledesign/slice/MainAbilitySlice.java
@@ -1,5 +1,6 @@
 package com.wgl.wearabledesign.slice;
 
+import com.wgl.configuration.CommandConfig;
 import com.wgl.configuration.HttpConfig;
 import com.wgl.wearabledesign.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
@@ -22,6 +23,8 @@ public class MainAbilitySlice extends AbilitySlice {
     private static final int HTTP_UPDATE = 1;
     private Timer timer;
     private TimerTask timerTask;
+    private CommandConfig lightCommand;
+    private CommandConfig motorCommand;
 
     private EventHandler eventHandler = new EventHandler(EventRunner.current()){
         protected void processEvent(InnerEvent event){
@@ -58,8 +61,12 @@ public class MainAbilitySlice extends AbilitySlice {
     public void init(){
         try {
             URL url = new URL("https://iotda.cn-north-4.myhuaweicloud.com/v5/iot/0c1907218a80f2952fa4c015c7ae499c/devices/607abeccba4b2702c056fe37_BearPiDemo/shadow");
+            URL commandURL = new URL("https://iotda.cn-north-4.myhuaweicloud.com/v5/iot/0c1907218a80f2952fa4c015c7ae499c/devices/607abeccba4b2702c056fe37_BearPiDemo/commands");
             String X_Auth_Token = "MIIVzgYJKoZIhvcNAQcCoIIVvzCCFbsCAQExDTALBglghkgBZQMEAgEwghPgBgkqhkiG9w0BBwGgghPRBIITzXsidG9rZW4iOnsiZXhwaXJlc19hdCI6IjIwMjEtMDQtMTlUMTM6MzM6MDEuOTYwMDAwWiIsIm1ldGhvZHMiOlsicGFzc3dvcmQiXSwiY2F0YWxvZyI6W10sInJvbGVzIjpbeyJuYW1lIjoidGVfYWRtaW4iLCJpZCI6IjAifSx7Im5hbWUiOiJ0ZV9hZ2VuY3kiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jc2UyLjBfY2ZnIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZGNzX21zX3J3cyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc19zcG90X2luc3RhbmNlIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfaXZhc192Y3JfdmNhIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZWNzX2FzY2VuZF9rYWkxIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZWNzX2thZTEiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9kYnNfcmkiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9ldnNfZXNzZCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc19ncHVfdjEwMCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2NzZTIuMF9ndm4iLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9kd3NfcG9jIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfY2JyX2ZpbGUiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3Nfa2MxX3VzZXJfZGVmaW5lZCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX21lZXRpbmdfZW5kcG9pbnRfYnV5IiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfbWFwX25scCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3Npc19zYXNyX2VuIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfc2FkX2JldGEiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9zZXJ2aWNlc3RhZ2VfbWdyX2R0bV9lbiIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX1ZJU19JbnRsIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZWNzX2dwdV9wMnMiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9ldnNfdm9sdW1lX3JlY3ljbGVfYmluIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZGNzX2RjczItZW50ZXJwcmlzZSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2NzZTIuMCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3ZjcCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2N2ciIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc19jNm5lIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYXBwY3ViZSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX211bHRpX2JpbmQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9vcF9nYXRlZF9pb3RzdGFnZSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2VpcF9wb29sIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfcm9tYWV4Y2hhbmdlIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9hcC1zb3V0aGVhc3QtM2QiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9wcm9qZWN0X2RlbCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3NoYXJlQmFuZHdpZHRoX3FvcyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2NjaV9vY2VhbiIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2V2c19yZXR5cGUiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9vbmVhY2Nlc3MiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3NfaXIzeCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2VsYl9ndWFyYW50ZWVkIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9jbi1zb3V0aHdlc3QtMmIiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jaWUiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9zZnN0dXJibyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3ZwY19uYXQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF92cG5fdmd3X2ludGwiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9odl92ZW5kb3IiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9hX2NuLW5vcnRoLTRlIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9jbi1ub3J0aC00ZCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc19ncHVfZzZ2IiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfSUVDIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZGF5dV9kbG1fY2x1c3RlciIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2ludGxfY29uZmlndXJhdGlvbiIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX25scF9sZ190ZyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2FfY24tbm9ydGgtNGYiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jcGgiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9tdGQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3NfZ3B1X2c1ciIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3drc19rcCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2NjaV9rdW5wZW5nIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfcmlfZHdzIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9jbi1zb3V0aHdlc3QtMmQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF92cGNfZmxvd19sb2ciLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9hYWRfYmV0YV9pZGMiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jc2JzX3JlcF9hY2NlbGVyYXRpb24iLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9kc3NfbW9udGgiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jc2ciLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9kZWNfbW9udGhfdXNlciIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2llZl9lZGdlYXV0b25vbXkiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF92aXBfYmFuZHdpZHRoIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfb3NjIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZWNzX29sZF9yZW91cmNlIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfd2VsaW5rYnJpZGdlX2VuZHBvaW50X2J1eSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Rjc19yaSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2llZi1pbnRsIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfcHN0bl9lbmRwb2ludF9idXkiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9tYXBfb2NyIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZGx2X29wZW5fYmV0YSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2llcyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX29ic19kdWFsc3RhY2siLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lZGNtIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfY3Nic19yZXN0b3JlIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfaXZzY3MiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3NfYzZhIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfdnBuX3ZndyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3Ntbl9jYWxsbm90aWZ5IiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfZG1zX3JlbGlhYmlsaXR5IiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfcGNhIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfY2NlX2FzbV9oayIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2NzYnNfcHJvZ3Jlc3NiYXIiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3Nfb2ZmbGluZV9waTIiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9ldnNfcG9vbF9jYSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX05BSUUiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9iY2UiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lY3Nfb2ZmbGluZV9kaXNrXzQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9lcHMiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9jc2JzX3Jlc3RvcmVfYWxsIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfbDJjZyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2ludGxfdnBjX25hdCIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Zjc19wYXkiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9sMmNnX2ludGwiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9hX2FwLXNvdXRoZWFzdC0xZSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2FfcnUtbW9zY293LTFiIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9hcC1zb3V0aGVhc3QtMWQiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9hX2FwLXNvdXRoZWFzdC0xZiIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc192Z3B1X2c1IiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfb3BfZ2F0ZWRfbWVzc2FnZW92ZXI1ZyIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX3RpY3Nfb3Blbl9iZXRhIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfbWFwX3Zpc2lvbiIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2Vjc19yaSIsImlkIjoiMCJ9LHsibmFtZSI6Im9wX2dhdGVkX2FfYXAtc291dGhlYXN0LTFjIiwiaWQiOiIwIn0seyJuYW1lIjoib3BfZ2F0ZWRfYV9ydS1ub3J0aHdlc3QtMmMiLCJpZCI6IjAifSx7Im5hbWUiOiJvcF9nYXRlZF9pZWZfcGxhdGludW0iLCJpZCI6IjAifV0sInByb2plY3QiOnsiZG9tYWluIjp7Im5hbWUiOiJodzI4OTg0MTMyIiwiaWQiOiIwOGFjNjc1NzVhODBmMzg4MGY2NGMwMTUyZTZhNWM4MCJ9LCJuYW1lIjoiY24tbm9ydGgtNCIsImlkIjoiMGMxOTA3MjE4YTgwZjI5NTJmYTRjMDE1YzdhZTQ5OWMifSwiaXNzdWVkX2F0IjoiMjAyMS0wNC0xOFQxMzozMzowMS45NjAwMDBaIiwidXNlciI6eyJkb21haW4iOnsibmFtZSI6Imh3Mjg5ODQxMzIiLCJpZCI6IjA4YWM2NzU3NWE4MGYzODgwZjY0YzAxNTJlNmE1YzgwIn0sIm5hbWUiOiJodzI4OTg0MTMyIiwicGFzc3dvcmRfZXhwaXJlc19hdCI6IiIsImlkIjoiMDhhYzY3NTg0MjgwMGY0MTFmN2ZjMDE1MWQyYWFmNzUifX19MYIBwTCCAb0CAQEwgZcwgYkxCzAJBgNVBAYTAkNOMRIwEAYDVQQIDAlHdWFuZ0RvbmcxETAPBgNVBAcMCFNoZW5aaGVuMS4wLAYDVQQKDCVIdWF3ZWkgU29mdHdhcmUgVGVjaG5vbG9naWVzIENvLiwgTHRkMQ4wDAYDVQQLDAVDbG91ZDETMBEGA1UEAwwKY2EuaWFtLnBraQIJANyzK10QYWoQMAsGCWCGSAFlAwQCATANBgkqhkiG9w0BAQEFAASCAQAG7MPcYKkfEQeXmCbDkLi213Js6qqHenR7sq-T-nWcuNnRfR3XsOkC72cRGHZvOukOHEUoyUwR2mzsX0yMbV46kU7YtOEuYWHdOZbk2EiS5iJlO2lDA56MYTtsGU0+3wYG2qvPlkuQKp5gpwbH64U-pxW5M62qgnRv-fKEV6qopCYeopH-YQumiTQ-FlT1s9QdHuHO+jWJO0Bquny3jck+IjQu-YaopsffewGbvei53MssBgkVazjEhxfHahHy03VDTo9ZAVgsjsCOxNHz0E-bMepgPKLveuFRem4X4X2kmEbBZSbErw7fp6SJJ194STLRO37pyJqEIaAdMBJYmSYc";
             config = new HttpConfig(url, X_Auth_Token);
+            lightCommand = new CommandConfig(commandURL, CommandConfig.CommandName.Agriculture_Control_light, CommandConfig.CommandType.Light, X_Auth_Token);
+            motorCommand = new CommandConfig(commandURL, CommandConfig.CommandName.Agriculture_Control_Motor, CommandConfig.CommandType.Motor, X_Auth_Token);
+
             System.out.println("Enter connect");
             Thread httpConfigThread = new Thread(config);
             httpConfigThread.start();
@@ -90,8 +97,30 @@ public class MainAbilitySlice extends AbilitySlice {
             public void onCheckedChanged(AbsButton absButton, boolean b) {
                 if(absButton.isChecked()){
                     System.out.println("LightCheck");
-                }else{
+                    lightCommand.setCommand(CommandConfig.Command.ON);
+                    Thread lightCommandThread = new Thread(lightCommand);
+                    lightCommandThread.start();
+                    try {
+                        lightCommandThread.join(5000);
+
+                        lightSwitch.setChecked(true);
+                        absButton.setChecked(true);
+                        System.out.println("LightOnSuccess!");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else if (!absButton.isChecked() && absButton.isPressed()){
                     System.out.println("LightOFFCheck");
+                    lightCommand.setCommand(CommandConfig.Command.OFF);
+                    Thread lightCommandThread = new Thread(lightCommand);
+                    lightCommandThread.start();
+                    try {
+                        lightCommandThread.join(5000);
+                        lightSwitch.setChecked(false);
+                        System.out.println("LightOFFSuccess!");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -99,10 +128,34 @@ public class MainAbilitySlice extends AbilitySlice {
         motorSwitch.setCheckedStateChangedListener(new AbsButton.CheckedStateChangedListener() {
             @Override
             public void onCheckedChanged(AbsButton absButton, boolean b) {
-                if(absButton.isChecked()){
+                if(absButton.isChecked() && absButton.isPressed()){
                     System.out.println("MotorCheck");
-                }else{
+                    motorCommand.setCommand(CommandConfig.Command.ON);
+                    Thread motorCommandThread = new Thread(motorCommand);
+                    motorCommandThread.start();
+                    try {
+                        motorCommandThread.join(5000);
+                        motorSwitch.setChecked(true);
+                        absButton.setChecked(true);
+                        System.out.println("MotorOnSuccess!");
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }else if(!absButton.isChecked() && absButton.isPressed()){
                     System.out.println("MotorCheck");
+                    motorCommand.setCommand(CommandConfig.Command.OFF);
+                    Thread motorCommandThread = new Thread(motorCommand);
+                    motorCommandThread.start();
+                    try {
+                        motorCommandThread.join(5000);
+
+                        motorSwitch.setChecked(false);
+                        absButton.setChecked(false);
+                        System.out.println("MotorOffSuccess");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
